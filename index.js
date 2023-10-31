d2a57171d2b72a67afbf982fa2638ca26c7cdd80
Rizwan Ahmad <113388795+Riz1981@users.noreply.github.com>
2023-10-31 11:44:58 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+//Number Eight result to each:
+//sum of 2 numbers
+let a = 4 + 4;
+console.log(" sum number = " + a);
+//subtraction
+let b = 10 - 2;
+console.log(" sub number = " + b);
+//multiplication
+let c = 2 * 4;
+console.log(" multi number = " + c);
+//division
+let d = 16 / 2;
+console.log(" divide number = " + d);
---
+++ index.ts
@@ -0,0 +1,18 @@
+//Number Eight result to each:
+//sum of 2 numbers
+let a:number=4+4;
+console.log(" sum number = " + a);
+//subtraction
+let b :number = 10-2;
+console.log(" sub number = " + b);
+//multiplication
+let c : number = 2*4;
+console.log(" multi number = " + c);
+//division
+let d : number = 16/2;
+console.log(" divide number = " + d);
+
+
+
+
+
